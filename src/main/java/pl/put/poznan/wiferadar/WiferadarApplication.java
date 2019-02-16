dd366dedaa337facb3ac29e33c1144f99e9e1cca
aleksandra <aleksandrabugaj@onet.eu>
2019-02-16 18:07:40 +0100

    WR-0001 init: Initialization of wiferadar repository
    Started project for mate from Poznań University of Technology. App base on web flux, vavr and lombok. Because of laziness done without any persistence.

---
+++ src/main/java/pl/put/poznan/wiferadar/WiferadarApplication.java
@@ -0,0 +1,14 @@
+package pl.put.poznan.wiferadar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiferadarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiferadarApplication.class, args);
+	}
+
+}
+
